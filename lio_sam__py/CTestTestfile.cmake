0222590e2cd47459ae1140cb71c27bac1eaeb08c
Aarushi Doctor <62117123+aarushi0998@users.noreply.github.com>
2024-08-12 19:23:58 -0600

    Delete lio_sam__py directory

--- lio_sam__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aria/lidarslam_ws/src/LIO-SAM/build/lio_sam/lio_sam__py
-# Build directory: /home/aria/lidarslam_ws/src/LIO-SAM/build/lio_sam/lio_sam__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
